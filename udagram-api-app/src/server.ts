37e3304f9283a399c9ad3b8e8446b89342172c66
Trí Phan <tripv1@unifiedpost.com>
2023-04-15 15:55:58 +0700

    add cmt cors

--- udagram-api-app/src/server.ts
+++ udagram-api-app/src/server.ts
@@ -23,7 +23,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
-
+  //cors
   app.use(cors());
 
   app.use("/api/v0/", IndexRouter);
