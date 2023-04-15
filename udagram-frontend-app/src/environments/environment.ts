c1bfece30a9d51e6a0e151421349af196db3a5c4
Trí Phan <tripv1@unifiedpost.com>
2023-04-15 14:06:13 +0700

    config env

--- udagram-frontend-app/src/environments/environment.ts
+++ udagram-frontend-app/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udacitytripv1app-env.eba-sjnqaq8x.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
